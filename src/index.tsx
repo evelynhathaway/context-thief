1b49c779a241a02c59c5db366a22dda916ee9f85
Evelyn Hathaway <git@evelyn.dev>
2021-05-24 19:05:44 -0700

    Upgrade(dev): npm update, linting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, {useContext} from "react";
 
 
+// eslint-disable-next-line unicorn/prevent-abbreviations
 export interface ContextValueRef<ContextValue> {
 	current?: ContextValue;
 }
@@ -10,12 +11,14 @@ const createContextThief = <ContextValue extends unknown> (context: React.Contex
 	contextValue: ContextValueRef<ContextValue>;
 } => {
 	const contextValue: ContextValueRef<ContextValue> = {};
+	const ContextThief: React.FC = ({children}) => {
+		const value = useContext(context);
+		contextValue.current = value;
+		// eslint-disable-next-line react/jsx-no-useless-fragment
+		return <>{children}</>;
+	};
 	return {
-		ContextThief: ({children}) => {
-			const value = useContext(context);
-			contextValue.current = value;
-			return <>{children}</>;
-		},
+		ContextThief,
 		contextValue,
 	};
 };
